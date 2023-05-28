d6fd4a36225be82c08da86ea72e6d52c6e5678d9
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-05-28 10:56:19 +0530

    bjkbhj

--- app/src/main/java/com/example/vtop_bhopal/mon_menu.java
+++ app/src/main/java/com/example/vtop_bhopal/mon_menu.java
@@ -1,14 +1,13 @@
 package com.example.vtop_bhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class mon_girl extends AppCompatActivity {
+public class mon_menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mon_girl);
+        setContentView(R.layout.activity_mon_menu);
     }
 }
